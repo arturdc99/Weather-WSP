79a6fc5faa095af1f57ae0f8a18f599c6aa4af7c
Luis Diuza <artur145@hotmail.com>
2018-04-09 22:38:08 -0500

    added front project

---
+++ Weather-WSP/WeatherFront/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeatherFront.Startup))]
+namespace WeatherFront
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
